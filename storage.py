cfff533e32295f8b93f8f4d0c1bf6ce8485b2b61
AndreiLeal2001 <102754659+AndreiLeal2001@users.noreply.github.com>
2025-07-11 05:18:08 -0300

    correção de bug

--- storage.py
+++ storage.py
@@ -5,7 +5,7 @@ ARQUIVO = "tarefas.json"
 
 def salvar_tarefas(tarefas):
     """Salvar a lista de tarefas em formato JSON."""
-    with open(ARQUIVO, 'w', enconding="utf-8") as arquivo:
+    with open(ARQUIVO, 'w', encoding="utf-8") as arquivo:
         json.dump(tarefas, arquivo, ensure_ascii=False, indent=4)
 
 def carregar_tarefas():
